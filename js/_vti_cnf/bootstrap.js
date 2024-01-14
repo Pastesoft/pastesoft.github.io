8dc589305c265962deda5d9177780bcb0cba0880
palutz <stefano13@duck.com>
2024-01-14 17:46:07 +0000

    v2

--- js/_vti_cnf/bootstrap.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|13 Jan 2014 14:41:44 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|PASTESOFT8\\vita
-vti_modifiedby:SR|PASTESOFT8\\vita
-vti_timecreated:TR|13 Jan 2014 14:41:44 -0000
-vti_cacheddtm:TX|13 Jan 2014 14:41:44 -0000
-vti_filesize:IR|58533
-vti_backlinkinfo:VX|
--- js/_vti_cnf/jquery-1.10.2.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|22 Nov 2013 23:16:40 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|PASTESOFT8\\vita
-vti_modifiedby:SR|PASTESOFT8\\vita
-vti_timecreated:TR|22 Nov 2013 23:16:40 -0000
-vti_cacheddtm:TX|22 Nov 2013 23:16:40 -0000
-vti_filesize:IR|93104
-vti_backlinkinfo:VX|
